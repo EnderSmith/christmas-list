7646298032419a2d90ae5ec1994a4f4328bf3142
Ender Smith <ender@happyleviathan.com>
2017-12-23 14:11:48 -0800

    added "/family" & "/:family_id/:member_id" routes

---
+++ api/app/models/listItem.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ListItemSchema = new Schema({
+  title: String,
+  link: String,
+  purchased: Boolean,
+  locked: Boolean,
+  deleted: Boolean,
+  updated: String,
+  updatedBy: String
+});
+
+module.exports = mongoose.model('ListItem', ListItemSchema);
--- api/app/models/member.js
+++ api/app/models/member.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const MemberSchema = new Schema({
-  memberId: String,
   name: String,
   email: String,
   parent: Boolean,
